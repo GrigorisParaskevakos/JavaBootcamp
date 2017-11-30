8d9a8e8e4f8c89c692e2f71ed995e56786d0730c
GrigorisParaskevakos <paraskevakosg@gmail.com>
2017-11-30 11:56:06 +0200

    Chech user existance

--- Catalog_java/DataBaseAccess.java
+++ Catalog_java/DataBaseAccess.java
@@ -20,7 +20,7 @@ public class DataBaseAccess {
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
-            //here testdb is database name, root is username and password
+            //here catalog is database name, root is username and password
             try (Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/catalog", "root", "12345")) {
 
                 Statement stmt = con.createStatement();
@@ -38,7 +38,7 @@ public class DataBaseAccess {
                     String userName = input.nextLine();
                     System.out.println("Lastname: ");
                     String lastName = input.nextLine();
-                    ResultSet rs2 = stmt2.executeQuery("SELECT * FROM catalog.users where first_name = '" + userName + "' AND Last_name = '" + lastName + "'" );
+                    ResultSet rs2 = stmt2.executeQuery("SELECT * FROM catalog.users where first_name = '" + userName + "' AND Last_name = '" + lastName + "'");
                     System.out.println();
                     if ((userName.equals(rs.getString(2))) && (lastName.equals(rs.getString(3)))) {
                         System.out.println("Hello" + userName + " \n");
@@ -55,25 +55,8 @@ public class DataBaseAccess {
                     input.close();
                     //exit(1);
                 }
-
-                //returns all users names
-                //ResultSet us = stmt.executeQuery("select 'first_name', 'last_name' from catalog.users");
-//                while (rs.next()) {
-//                    //Display Values
-//                    System.out.println("USER ID| FIRST NAME  | LAST NAME       | PHONE1    | PHONE2");
-//                    System.out.printf(rs.getInt(1) + "      | " + rs.getString(2) + "       | " + rs.getString(3) + " | " + rs.getString(4) + "| " + rs.getString(5) + "\n");
-//                    System.out.println("----------------------------");
-//                }
-//                con.close();
                 con.close();
 
-                /*while (user1.next()) {
-                    //System.out.println("user me ID 1: " + user1.getString(1));
-                    if ("admin".equals(user1.getString(1))) {
-                        System.out.println("admin connected");
-                        //more code here
-                    }
-                }*/
             } catch (Exception e) {
                 System.out.println(e);
             }
