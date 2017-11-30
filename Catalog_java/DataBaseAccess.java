d871b0aebda7a8f1bb33767e82f2e6fef9ec73a7
GrigorisParaskevakos <paraskevakosg@gmail.com>
2017-11-30 11:54:22 +0200

    Chech user existance

--- Catalog_java/DataBaseAccess.java
+++ Catalog_java/DataBaseAccess.java
@@ -5,8 +5,10 @@ package catalog;
  * @author AsusGL702Vm
  */
 import java.io.FileNotFoundException;
+import static java.lang.System.exit;
 import java.sql.*;
 import java.sql.DriverManager;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,28 +30,43 @@ public class DataBaseAccess {
 
                 ResultSet rs = stmt.executeQuery("select * from catalog.users");
 
-                //ResultSet user1 = stmt4.executeQuery("select testdb.users.username from  testdb.users where testdb.users.username = 'admin'");
-//                System.out.println("View: users ammount");
-//                System.out.println("ID| USER | AMMOUNT");
-//                System.out.println("----------------------------");
-//                while (user_ammount.next()) {
-//                    System.out.printf(user_ammount.getInt(1) + " | " + user_ammount.getString(2) + "|" + user_ammount.getDouble(3) + "\n");
+                //ask user
+                Scanner input = new Scanner(System.in);
+                int i = 0;
+                while (rs.next()) {
+                    System.out.println("FirstName: ");
+                    String userName = input.nextLine();
+                    System.out.println("Lastname: ");
+                    String lastName = input.nextLine();
+                    ResultSet rs2 = stmt2.executeQuery("SELECT * FROM catalog.users where first_name = '" + userName + "' AND Last_name = '" + lastName + "'" );
+                    System.out.println();
+                    if ((userName.equals(rs.getString(2))) && (lastName.equals(rs.getString(3)))) {
+                        System.out.println("Hello" + userName + " \n");
+                        while (rs2.next()) {
+                            //Display Values
+                            System.out.println("USER ID| FIRST NAME  | LAST NAME       | PHONE1    | PHONE2");
+                            System.out.printf(rs.getInt(1) + "      | " + rs.getString(2) + "       | " + rs.getString(3) + " | " + rs.getString(4) + "| " + rs.getString(5) + "\n");
+                            System.out.println("----------------------------");
+                        }
+                    } else {
+                        i++;
+                        System.out.println("You don't Exist, sorry!\n");
+                    }
+                    input.close();
+                    //exit(1);
+                }
+
+                //returns all users names
+                //ResultSet us = stmt.executeQuery("select 'first_name', 'last_name' from catalog.users");
+//                while (rs.next()) {
+//                    //Display Values
+//                    System.out.println("USER ID| FIRST NAME  | LAST NAME       | PHONE1    | PHONE2");
+//                    System.out.printf(rs.getInt(1) + "      | " + rs.getString(2) + "       | " + rs.getString(3) + " | " + rs.getString(4) + "| " + rs.getString(5) + "\n");
 //                    System.out.println("----------------------------");
 //                }
-                while (rs.next()) {
-                    //Retrieve by column name
-//                    int userid = rs.getInt("user_id");
-//                    String firstname = rs.getString("first_name");
-//                    String lastname = rs.getString("last_name");
-//                    String phone1 = rs.getString("phone1");
-//                    String phone2 = rs.getString("phone2");
-
-                    //Display Values
-                    System.out.println("USER ID| FIRST NAME  | LAST NAME       | PHONE1    | PHONE2");
-                    System.out.printf(rs.getInt(1) + "      | " + rs.getString(2) + "       | " + rs.getString(3) + " | " + rs.getString(4) + "| " + rs.getString(5) + "\n");
-                    System.out.println("----------------------------");
-                }
+//                con.close();
                 con.close();
+
                 /*while (user1.next()) {
                     //System.out.println("user me ID 1: " + user1.getString(1));
                     if ("admin".equals(user1.getString(1))) {
